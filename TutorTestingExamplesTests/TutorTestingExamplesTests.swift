4ce4b4270bcc24124dea3a3b5a2d3c9684f92d0c
Alexis <alexisgenieve@gmail.com>
2024-09-26 13:12:04 -0600

    Initial Commit

---
+++ TutorTestingExamplesTests/TutorTestingExamplesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TutorTestingExamplesTests.swift
+//  TutorTestingExamplesTests
+//
+//  Created by Alexis Wright on 9/26/24.
+//
+
+import Testing
+@testable import TutorTestingExamples
+
+struct TutorTestingExamplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
